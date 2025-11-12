eb00c1053003cd9cdf2f0e4326068d5e407ac7bc
Ben Johnson-Staub <benjamin.johnsonstaub@datadoghq.com>
2025-11-12 14:04:19 -0500

    Tests

--- azure/shared/tests/test_data.py
+++ azure/shared/tests/test_data.py
@@ -7,3 +7,6 @@
 CONTROL_PLANE_SUBSCRIPTION_ID = "cp-sub-id"
 CONTROL_PLANE_REGION = "eastus"
 CONTROL_PLANE_RESOURCE_GROUP = "test-rg"
+
+EXAMPLE_POLICY_NAME = "Example policy name"
+EXAMPLE_POLICY_ERROR = """Resource 'dd-log-forwarder-env-00000000-eastus' was disallowed by policy. Policy identifiers: '[{"policyAssignment":{"name":"Example policy assignment","id":"/providers/Microsoft.Management/managementGroups/mgroup1/providers/Microsoft.Authorization/policyAssignments/1111111"},"policyDefinition":{"name":"Example policy name","id":"/providers/Microsoft.Authorization/policyDefinitions/222222222222222","version":"1.1.0"},"policySetDefinition":{"name":"Example policy set","id":"/providers/Microsoft.Management/managementGroups/SomeMGroup/providers/Microsoft.Authorization/policySetDefinitions/333333333333333333","version":"1.0.0"}}]'."""
