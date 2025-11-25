6e85428267dc2cbfc5a8ede73a1ac08d2d3d3392
Ben Johnson-Staub <benjamin.johnsonstaub@datadoghq.com>
2025-11-25 15:43:41 -0500

    Test action container

--- azure/integration_quickstart/tests/test_actions.py
+++ azure/integration_quickstart/tests/test_actions.py
@@ -4,7 +4,7 @@
 
 from unittest import TestCase
 
-from azure_integration_quickstart.actions import Action, is_action_lte, is_action_overlapping
+from azure_integration_quickstart.actions import Action, ActionContainer, is_action_lte, is_action_overlapping
 
 
 class TestActions(TestCase):
@@ -16,9 +16,9 @@ class TestActions(TestCase):
             ("unrelated", "*/read", "Microsoft.Compute/virtualMachines/*", False),
         ]
 
-        for name, scope_a, scope_b, expected_result in test_cases:
+        for name, action_a, action_b, expected_result in test_cases:
             with self.subTest(msg=name):
-                actual = is_action_lte(scope_a, scope_b)
+                actual = is_action_lte(action_a, action_b)
                 self.assertEqual(actual, expected_result)
 
     def test_is_action_overlapping(self):
@@ -29,7 +29,69 @@ class TestActions(TestCase):
             ("unrelated", "*/read", "Microsoft.Compute/virtualMachines/*", False),
         ]
 
-        for name, scope_a, scope_b, expected_result in test_cases:
+        for name, action_a, action_b, expected_result in test_cases:
             with self.subTest(msg=name):
-                actual = is_action_overlapping(scope_a, scope_b)
+                actual = is_action_overlapping(action_a, action_b)
+                self.assertEqual(actual, expected_result)
+
+    def test_contains_action(self):
+        test_cases: list[tuple[str, ActionContainer, Action, bool]] = [
+            ("empty", ActionContainer([], []), "Microsoft.Compute/virtualMachines/read", False),
+            (
+                "no match",
+                ActionContainer(["Microsoft.Compute/virtualMachines/write"], []),
+                "Microsoft.Compute/virtualMachines/read",
+                False,
+            ),
+            (
+                "exact match",
+                ActionContainer(
+                    ["Microsoft.Compute/virtualMachines/write", "Microsoft.Compute/virtualMachines/read"], []
+                ),
+                "Microsoft.Compute/virtualMachines/read",
+                True,
+            ),
+            (
+                "lt match",
+                ActionContainer(["Microsoft.Compute/virtualMachines/*"], []),
+                "Microsoft.Compute/virtualMachines/read",
+                True,
+            ),
+            (
+                "multiple matches",
+                ActionContainer(["Microsoft.Compute/virtualMachines/read", "Microsoft.Compute/virtualMachines/*"], []),
+                "Microsoft.Compute/virtualMachines/read",
+                True,
+            ),
+            (
+                "greater not action",
+                ActionContainer(["Microsoft.Compute/virtualMachines/read"], ["Microsoft.Compute/*/*"]),
+                "Microsoft.Compute/virtualMachines/*",
+                False,
+            ),
+            (
+                "less than not action",
+                ActionContainer(
+                    ["Microsoft.Compute/virtualMachines/read"], ["Microsoft.Compute/virtualMachines/write"]
+                ),
+                "Microsoft.Compute/virtualMachines/*",
+                False,
+            ),
+            (
+                "equal not action",
+                ActionContainer([], ["Microsoft.Compute/virtualMachines/*"]),
+                "Microsoft.Compute/virtualMachines/*",
+                False,
+            ),
+            (
+                "unrelated not action",
+                ActionContainer(["Microsoft.Compute/virtualMachines/*"], ["Microsoft.Compute/analogMachines/read"]),
+                "Microsoft.Compute/virtualMachines/read",
+                True,
+            ),
+        ]
+
+        for name, container, action, expected_result in test_cases:
+            with self.subTest(msg=name):
+                actual = action in container
                 self.assertEqual(actual, expected_result)
