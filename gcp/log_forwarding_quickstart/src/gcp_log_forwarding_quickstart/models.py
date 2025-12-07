1b70dc7d3cb0fc9746745f8138a80a3564873d1a
Teddy Kahwaji <teddy.kahwaji@datadoghq.com>
2025-12-07 12:55:10 -0500

    update service account id for dataflow configuration

--- gcp/log_forwarding_quickstart/src/gcp_log_forwarding_quickstart/models.py
+++ gcp/log_forwarding_quickstart/src/gcp_log_forwarding_quickstart/models.py
@@ -3,7 +3,6 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/) Copyright 2025 Datadog, Inc.
 
 from dataclasses import dataclass
-
 from typing import Optional
 
 
