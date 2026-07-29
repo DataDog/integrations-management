e2bfc98f8f3998693d6f5f5d19b0db20130604ff
EamonBrady1 <eamon.brady@datadoghq.com>
2026-07-29 14:59:17 -0400

    Raise EPM token generator fallback min length to 14
    Default kicks in only when the tenant password policy fetch fails or
    omits minLength.

--- oracle_fusion/integration_quickstart/setup.sh
+++ oracle_fusion/integration_quickstart/setup.sh
@@ -1022,7 +1022,7 @@ except Exception:
     resources = []
 policy = next((r for r in resources if 'default' in r.get('name', '').lower()), resources[0] if resources else {})
 
-min_length = int(get(policy, 'minLength', 'min-length', default=8))
+min_length = int(get(policy, 'minLength', 'min-length', default=14))
 max_length = int(get(policy, 'maxLength', 'max-length', default=20))
 min_lower = int(get(policy, 'minLowerCase', 'min-lower-case', default=1))
 min_upper = int(get(policy, 'minUpperCase', 'min-upper-case', default=1))
