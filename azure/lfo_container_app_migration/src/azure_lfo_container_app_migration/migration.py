eda1e04c62a60b385121951a33b287b245dcb102
Rebecca Parsons <rebecca.parsons@datadoghq.com>
2026-09-09 16:44:22 -0400

    [AZINTS-4618] CAJ migration script - Create role assignments + add pyz (#232)
    * Add option to filter find_existing_lfo_control_planes to a type
    
    * Add get_current_config_for_control_plane
    
    * add generic functions to check resource existance
    
    * Finish moving task names and image names to ControlPlane
    
    * fix tests
    
    * Add Step class
    
    * add skeleton for migration
    
    * Add main.py and boilerplate
    
    * comments
    
    * wip create new CAJs
    
    * ruff
    
    * build.sh
    
    * fix signature
    
    * fix optional control plane IDs
    
    * add handling around monitored_sub_ids_str
    
    * build.sh
    
    * Create CAJs based on existing function apps
    
    * cleanup
    
    * build.sh
    
    * grant roles for tasks
    
    * add pyz for migration script
    
    * improve logs
    
    * pyz
    
    * simplify gitignore
    
    * build.sh
    
    * other build
    
    * try build.sh again
    
    * try changing gitignore

--- azure/lfo_container_app_migration/src/azure_lfo_container_app_migration/migration.py
+++ azure/lfo_container_app_migration/src/azure_lfo_container_app_migration/migration.py
@@ -12,12 +12,13 @@ from .create_container_app_job import (
     CreateResourcesTaskContainerAppJob,
     CreateScalingTaskContainerAppJob,
 )
+from .grant_permissions import GrantContainerAppJobPermissionsStep
 from .prompts import confirm_yes
 from .steps import run_steps, Step
 
 
 
-def run_migration(optional_control_plane_ids: set[str], skip_confirmation: bool,  log_level: str) -> None:
+def run_migration(optional_control_plane_ids: set[str], skip_confirmation: bool, log_level: str) -> None:
     control_planes: list[ControlPlane] = find_existing_lfo_control_planes(subscriptions=None, control_plane_type=ControlPlaneType.FunctionApps)
     if len(optional_control_plane_ids) > 0:
         control_planes = [c for c in control_planes if c.id in optional_control_plane_ids]
@@ -47,8 +48,7 @@ def run_migration(optional_control_plane_ids: set[str], skip_confirmation: bool,
 
 def migrate_control_plane(control_plane: ControlPlane, log_level: str) -> None:
     log_header(
-        f"Migrating control plane {control_plane.id} "
-        f"({control_plane.sub_id} / {control_plane.resource_group})"
+        f"Migrating control plane {control_plane.id} in resource group {control_plane.resource_group}) subscription {control_plane.sub_id}"
     )
 
     function_app_config = get_current_config_for_control_plane(control_plane)
@@ -63,6 +63,7 @@ def _build_migration_steps(caj_config: Configuration) -> list[Step]:
         CreateResourcesTaskContainerAppJob(caj_config),
         CreateDiagnosticSettingsTaskContainerAppJob(caj_config),
         CreateScalingTaskContainerAppJob(caj_config),
+        GrantContainerAppJobPermissionsStep(caj_config),
     ]
 
 
